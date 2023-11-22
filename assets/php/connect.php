a130cfdfbf58f5f08e83b324b887b3316e593385
BlueBlizzardd <105523256+BlueBlizzardd@users.noreply.github.com>
2023-11-21 22:41:19 -0500

    BDD de prod.

--- assets/php/connect.php
+++ assets/php/connect.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = "localhost";
-$user = "root";
-$pass = "";
-$db = "icardb";
+$user = "id20904698_ejuliao";
+$pass = "Elmatadordebarquisimeto69.";
+$db = "id20904698_cansaoya";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
